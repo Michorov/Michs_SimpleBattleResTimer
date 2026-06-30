1e2c6c948ab5f50876c25d79524025ecf428239c
Mich <caedesque.apps@icloud.com>
2026-06-30 20:20:57 +0300

    Added UpdateSettings for Battle Res

--- BattleResTimer.lua
+++ BattleResTimer.lua
@@ -1,28 +1,24 @@
-local addonName, addon = ...
+local _, addon = ...
 
 local BattleResTimer = {}
 addon.BattleResTimer = BattleResTimer
 
-local BATTLE_RES_SPELL_ID = 20484
-local BATTLE_RES_FALLBACK_ICON = "Interface\\Icons\\Spell_Nature_Reincarnation"
-
 local iconFrame
-
-local function GetBattleResIcon()
-	if C_Spell and C_Spell.GetSpellTexture then
-		return C_Spell.GetSpellTexture(BATTLE_RES_SPELL_ID)
-	end
-
-	return BATTLE_RES_FALLBACK_ICON
-end
 
 function BattleResTimer:Initialize()
 	iconFrame = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-	iconFrame:SetSize(40, 40)
-	iconFrame:SetPoint("CENTER", UIParent, "CENTER")
-
 	iconFrame.icon = iconFrame:CreateTexture(nil, "ARTWORK")
 	iconFrame.icon:SetAllPoints(iconFrame)
-	iconFrame.icon:SetTexture(GetBattleResIcon())
+	iconFrame.icon:SetTexture("Interface\\Icons\\Spell_Nature_Reincarnation")
 	iconFrame.icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+
+	self:UpdateSettings()
+end
+
+function BattleResTimer:UpdateSettings()
+	iconFrame:SetSize(40, 40)
+	iconFrame:ClearAllPoints()
+	iconFrame:SetPoint("CENTER", UIParent, "CENTER")
+
+	iconFrame:Show()
 end
